6bfa03611c50a1492769f35e66ee5ae2e77367ed
Harry Scells <harryscells@gmail.com>
2015-08-31 23:01:24 +1000

    try to read in a file and evaluate it

--- main.c
+++ main.c
@@ -15,6 +15,8 @@ t_symboltable *symboltable;
 
 int main(int argc, char const *argv[]) {
 
+  // first couple of lines initialise the system itself:
+
   // initiate the type system
   initTypes();
   // create a heap for the vm to run
@@ -23,54 +25,79 @@ int main(int argc, char const *argv[]) {
   t_stack* mainStack = newStack();
   symboltable = newSymbolTable();
 
-  // testing....
-  t_object* shop = newObject();
-  addFieldToFieldlist(shop->fields, "money", Type->Int, newInt(420, mainStack, heap));
-
-  // z_println(shop->fields->field[0]->object);
   t_ast* ast = newAst();
-  ast->node = newTokenNode(ast->tokens->START, ast);
-  struct node* starting_node = ast->node;
 
-  ast->node->next = newTokenNode(ast->system->ADD, ast);
-  ast->node = ast->node->next;
+  // these next lines are used to read a file in for evaluation
 
-  ast->node->next = newTokenNode(ast->tokens->LBRAC, ast);
-  ast->node = ast->node->next;
+  char *buffer = 0;
+  long length;
+  FILE *f;
 
-  ast->node->next = newObjectNode(newInt(8, mainStack, heap), ast);
-  ast->node = ast->node->next;
+  if (argc > 1) {
+    f = fopen(argv[1], "rb");
 
-  ast->node->next = newTokenNode(ast->tokens->COMMA, ast);
-  ast->node = ast->node->next;
+    if (f) {
+      fseek(f, 0, SEEK_END);
+      length = ftell(f);
+      fseek(f, 0, SEEK_SET);
+      buffer = malloc(length);
+      if (buffer) {
+        fread(buffer, 1, length, f);
+      }
+      fclose(f);
+    }
 
-  ast->node->next = newObjectNode(newInt(5, mainStack, heap), ast);
-  ast->node = ast->node->next;
+  }
 
-  ast->node->next = newTokenNode(ast->tokens->RBRAC, ast);
-  ast->node = ast->node->next;
+  if (buffer) {
+    ast = parse(buffer, mainStack, heap, 0);
+    eval(ast);
+  } else {
 
-  ast->node = starting_node;
+    //  if there is no input, test some things
+    struct node* starting_node = ast->node;
 
-  eval(ast);
+    ast->node->next = newTokenNode(ast->system->ADD, ast);
+    ast->node = ast->node->next;
+
+    ast->node->next = newTokenNode(ast->tokens->LBRAC, ast);
+    ast->node = ast->node->next;
+
+    ast->node->next = newObjectNode(newInt(8, mainStack, heap), ast);
+    ast->node = ast->node->next;
+
+    ast->node->next = newTokenNode(ast->tokens->COMMA, ast);
+    ast->node = ast->node->next;
+
+    ast->node->next = newObjectNode(newInt(5, mainStack, heap), ast);
+    ast->node = ast->node->next;
+
+    ast->node->next = newTokenNode(ast->tokens->RBRAC, ast);
+    ast->node = ast->node->next;
+
+    ast->node->next = NULL;
+
+    ast->node = starting_node;
+
+    eval(ast);
+  }
+
+  // garbage function but resides in expressions
   collectAst(ast);
-  // repl_init();
 
   free(heap);
   free(mainStack);
   free(Type);
 
-  collect(shop);
-
   return 0;
 }
 
-void repl_init() {
+void repl_init(t_stack* stack, t_heap* heap) {
   char input[INPUT_SIZE] = "";
   printf("%s", ">>> ");
   while (EXIT_STATUS == 0 && fgets(input, INPUT_SIZE, stdin)) {
     t_expression *expression = (t_expression*) malloc(sizeof(t_expression));
-    expression->ast = parse(input, 0);
+    expression->ast = parse(input, stack, heap, 0);
 
     eval(expression->ast);
     free(expression->ast);
